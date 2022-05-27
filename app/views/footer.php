f5403bee32cb784832d3a99943e2e05f889cad17
l33dinh <dinh.le3006@hcmut.edu.vn>
2022-05-27 13:43:50 +0700

    [c] Design Footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,3 +1,79 @@
+<footer>
+    <div class="row">
+        <div class="col mt-3" >
+            <h1> WASSUP</h1>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.494668100784!2d106.65843061458902!3d10.773374292323592!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ec3c161a3fb%3A0xef77cd47a1cc691e!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBCw6FjaCBraG9hIC0gxJDhuqFpIGjhu41jIFF14buRYyBnaWEgVFAuSENN!5e0!3m2!1svi!2s!4v1653629953688!5m2!1svi!2s"
+                width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade" class="map"></iframe>
+
+            </div>
+        <div class="col">
+        <h3>Office</h3>
+            <p> <i class="fa fa-map-marker"  style="color:red"></i> 268 Ly Thuong Kiet Road, Ward 14, District 10</p> 
+            <p>Ho Chi Minh City, Viet Nam</p>
+            <p style="padding-top:10px"> <i class="fa fa-map-marker"  style="color:red"></i> 42 B2 Sala Urban Area, An Loi Dong Ward</p> 
+            <p>Thu Duc City, Viet Nam</p>
+            <p style="padding-top:10px"> <i class="fa fa-phone" style="color:green" ></i> (+84) 814-725-878 </p>
+            
+        </div>
+        <div class="col">
+            <h5>About</h5>
+        </div>
+    </div>
+
+
+</footer>
+<style>
+footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    background: rgba(97, 180, 159,1);
+    color: #fff;
+    padding: 70px 0 30px;
+    border-top-left-radius: 125px;
+    font-size: 13px;
+    line-height: 7px;
+    color: #000;
+}
+
+footer .map{
+    padding-top: 10px;
+    border-radius: 25px;
+
+}
+
+footer .row {
+    width: 85%;
+    margin: 5px 100px 0px 50px;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    justify-content: space-between;
+}
+
+footer .col {
+    flex-basis: 25%;
+    padding: 0px;
+}
+
+footer i{
+    position: relative;
+
+}
+
+footer .row p{
+    font-size: 1rem;
+}
+
+footer .col h3 {
+    width: fit-content;
+    margin-bottom: 40px;
+    position: relative;
+}
+</style>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -86,23 +86,7 @@
 
 
 
-<div class="news1 position-absolute bg-light slide1">
-    <object class="position-absolute" data="/assets/bg.svg">
-    </object>
-    <div class="row g-0 position-relative full-height">
-        <div class="col-md-6 p-4 ps-md-0 message-container">
-            <h1 class="message mt-0 display-1 p-4">EVERY CHOICE IS A JOURNEY</h1>
-            <p class="message lead p-4">Let us be your backpacker.</p>
-        </div>
-        <div class="col-md-6 mb-md-0 p-md-4 full-height">
-            <div class="img-descrip d-flex justify-content-center">
-                <img src="/assets/cloud.png" class="position-absolute top-50 translate-middle-y" alt="..." />
-                <img src="/assets/business-3d-man-lying-with-laptop.png"
-                    class="position-absolute top-50 translate-middle-y" alt="..." />
-            </div>
-        </div>
-    </div>
-</div>
+
 
 
 <?php
