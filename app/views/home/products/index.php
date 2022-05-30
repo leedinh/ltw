129b06e6c5502012b1ba86910203752ccacc23d5
nttvy2709 <vyngo2709@gmail.com>
2022-05-31 01:30:13 +0700

    Add brand carousel

--- app/views/home/products/index.php
+++ app/views/home/products/index.php
@@ -5,50 +5,157 @@
 	include($path.'header.php');
   include($path.'navbar.php');
 ?>
-  <div class="container">
-    <h3 class="h3">Products Grid</h3>
+<style>
+  body {
+    top: 10%;
+  }
+  img{
+        max-width: 100%;
+        max-height: 100%;
+        display: block; /* remove extra space below image */
+    }
+  .carousel{
+    width: 10vw;
+    height: auto;
+  }
+  .brands {
+    align-items: center;
+    justify-content: center;
+  }
+  .brands .box {
+    width: 10%;
+    height: 10%;
+  }
+  .brands .box-btn {
+    width: 6vw;
+    height: 6vw;
+  }
+  .bg-opacity{
+    background: rgba(255, 255, 255, 0.3);
+    border-radius: 25px;
+  }
+</style>
+  <div class="position-relative container w-75">
+    <!-- <img alt="LENOVO LEGION 5: Best Choice tầm giá 27 triệu!" src="https://media-api-beta.thinkpro.vn/media/core/banners/2022/5/12/Bản sao legion 5 866-374.jpg" class="blur-up h-full w-full object-cover t-img"> -->
     <div class="row">
-        <?php
-          foreach($data['product'] as $item){
-            echo '<div class="col-md-4 col-sm-2 mt-3">
-            <div class="product-grid4">
-                <div class="product-image4">
-                    <a href="/home/item/' . $item->pid .'">
-                        <img class="pic-1" src="/assets/products/Image3.jpg">
-                        <img class="pic-2" src="/assets/products/Image3_1.jpg">
-                    </a>
-                    <ul class="social">
-                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
-                        <li><a href="#" data-tip="Add to Wishlist"><i class="fa fa-shopping-bag"></i></a></li>
-                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
-                    </ul>
-                    <span class="product-new-label">New</span>
-                    <span class="product-discount-label">-10%</span>
+        <div class="col-4">
+          <div class="container brands d-flex mb-3">
+            <div id="carousel-example-multi" class="carousel slide carousel-multi-item v-2 product-carousel">
+              <div class="control">
+                <button class="btn btn-sm border border-outline-dark" data-bs-target="#carousel-example-multi" data-bs-slide="prev">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-up" viewBox="0 0 16 16">
+                    <path d="M3.204 11h9.592L8 5.519 3.204 11zm-.753-.659 4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z"/>
+                  </svg>
+                </button>
+              </div>
+                <div class="carousel-inner" role="listbox">
+                  <div class="carousel-item active mx-auto">
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/Asus.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/lenovo.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/hp.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                    <img class="" src="/assets/logo/dell.png" alt="">
+                  </button>
                 </div>
-                <div class="product-content">
-                    <h3 class="title">
-                    <a href="#">
-                        '.$item->manufacturer.' '.$item->model.' '.$item->category.' 
-                    </a>
-                    </h3>
-                    <div class="price">
-                       '.$item->price.'
-                        <span>$16.00</span>
-                    </div> 
-                    <btn type="button" class="add-to-cart">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
-                            <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
-                            <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                        </svg>
-                    </btn>
-                    <btn type="button" class="add-to-cart">Details</btn>
-        
+                <div class="carousel-item">
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/Google.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/acer.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/lg.svg" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                    <img class="" src="/assets/logo/samsung.png" alt="">
+                  </button>
+                </div>
+                <div class="carousel-item">
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/huawei.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/microsoft.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                      <img class="" src="/assets/logo/msi.png" alt="">
+                    </button>
+                    <button type="button" class="btn box-btn border border-outline-dark p-3 m-3">
+                    <img class="" src="/assets/logo/razer.svg" alt="">
+                  </button>
+                  </div>
+                  <!-- <div class="carousel-item ">
+                  </div> -->
+                </div>
+                <div class="control">
+                  <button class="btn btn-sm border border-outline-dark" data-bs-target="#carousel-example-multi" data-bs-slide="next">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-down" viewBox="0 0 16 16">
+                      <path d="M3.204 5h9.592L8 10.481 3.204 5zm-.753.659 4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z"/>
+                    </svg>
+                  </button>
                 </div>
             </div>
-        </div>';
-            
-          }
-        ?>
+          </div>
+        </div>
+        <div class="col-8 p-3 bg-opacity">
+          <div class="search-bar">
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Search...">
+          </div>
+          <div class="row p-3">
+              <?php
+                foreach($data['product'] as $item){
+                  echo '<div class="col-md-4 col-sm-2 mt-3" style="height: 35vh;">
+                  <div class="product-grid4 h-100">
+                      <div class="product-image4">
+                          <a href="/home/item/' . $item->pid .'">
+                              <img class="pic-1" src="/assets/products/Image3.jpg">
+                              <img class="pic-2" src="/assets/products/Image3_1.jpg">
+                          </a>
+                          <span class="product-new-label">New</span>
+                          <span class="product-discount-label">-10%</span>
+                      </div>
+                      <div class="product-content container">
+                          <h3 class="title" style="height: 15%;">
+                          <a href="#">
+                              '.$item->manufacturer.' '.$item->model.'
+                          </a>
+                          </h3>
+                          <div class="price container" style="height: 5%;">
+                             '.$item->price.'
+                          </div> 
+                          <div class="container table-btn" >
+                            <div class="row" >
+                              <div class="col">
+                                  <btn type="button" class="btn btn-outline-dark w-100 h-100">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+                                        <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
+                                        <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
+                                    </svg>
+                                  </btn>
+
+                              </div>
+                              <div class="col">
+                                  <btn type="button" class="btn btn-outline-dark w-100 h-100">Details</btn>
+                                </div>
+                              </div>
+
+                           </div> 
+              
+                      </div>
+                  </div>
+              </div>';
+                  
+                }
+              ?>
+          </div>
+        </div>
     </div>
 </div>
 <div class="position-fixed bottom-0 end-0 m-3">
@@ -70,6 +177,9 @@
   </div>
 </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+</script>
 <?php
 	// include($path.'footer.php');
 ?>
