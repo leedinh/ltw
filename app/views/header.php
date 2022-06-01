8cf7de0602a7652c83dd1304fa392c03566d0e4e
nttvy2709 <vyngo2709@gmail.com>
2022-06-01 23:32:06 +0700

    Add jquery

--- app/views/header.php
+++ app/views/header.php
@@ -11,5 +11,6 @@
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
 <body>
