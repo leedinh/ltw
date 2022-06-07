898c80d534a52a2389995a5160ae54f745de5c01
l33dinh <dinh.le3006@hcmut.edu.vn>
2022-06-07 23:50:56 +0700

    Fix cart

--- app/views/home/products/index.php
+++ app/views/home/products/index.php
@@ -170,7 +170,7 @@
                             </div> 
                             <div class=" table-btn mt-4 d-flex justify-content-center" >
                                 <div class="button w-100 d-flex justify-content-center">
-                                <btn type="button" class="btn btn-outline-dark w-75">
+                                <btn data-pid=' . $item->pid . ' type="button" class="btn cartbtn btn-outline-dark w-75">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
                                 <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
                                 <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
@@ -222,8 +222,8 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </svg>
-        <span class="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-dark">
-    99+
+        <span id="numi" class="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-dark">
+    0
     <span class="visually-hidden">unread messages</span>
   </span>
     </button>
@@ -269,6 +269,16 @@
     showSlice(slice);
   });
   
+  $('.cartbtn').click(function() {
+    $pid = $(this).data('pid');
+    $.ajax({
+      url: '/home/cartclick/' + $pid,
+      type: 'GET',
+      success: function(data) { 
+        $('#numi').html(data);
+      }
+    });
+  });
   
   $('.prev').click(function(){
     // console.log($curr);
